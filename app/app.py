0f265430818c21ffec964f80d02feb403a584cc7
Mahavsarojini <sthotasarojini@gmail.com>
2026-01-21 12:51:56 +0530

    Initialize Flask app with hello route

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World! CI/CD Pipeline with Jenkins, Docker, and AWS ECS 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
